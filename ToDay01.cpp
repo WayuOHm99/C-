fb20f4255dc8cae725970695503a3d932fd2f47c
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2023-11-30 15:26:44 +0700

    Update ToDay01.cpp

--- ToDay01.cpp
+++ ToDay01.cpp
@@ -1,19 +1,19 @@
 /* LANG : C++ */
 #include <iostream>
-
+#include <cmath>
+#include <stdio.h>
 using namespace std;
-
 int main ()
 {
-	int num1,num2,rem1,rem2 ;
+	int num1,num2,rem1,rem2;
 	cin >> num1;
 	cin >> num2;
 	rem1=num1%2;
 	rem2=num2%2;
-	if(rem1==0) cout << "Evan" << endl;
+	if(rem1==0) cout << "Even" << endl;
 	else cout << "Odd" << endl;
-	if(rem2==0) cout << "Evan" << endl;
+	if(rem2==0) cout << "Even" << endl;
 	else cout<< "Odd" << endl;
-	
+
 	return 0;
 }
